ecf1173c9e39787a8b5ba6640a000ed1ee12bc55
Kacper Odzimek <105165506+TakiKacper@users.noreply.github.com>
2025-09-20 16:30:52 +0200

    Batches

--- include/vine/vine.hpp
+++ include/vine/vine.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <initializer_list>
 #include <any>
+#include <vector>
+#include <initializer_list>
 
 #define DELETE_MOVE_COPY(class_name)                    \
     class_name(const class_name&)            = delete;  \
@@ -14,7 +15,38 @@
 // Compile Flags
 
 //VINE_MAX_THREADS - max number of thread workers
-    
+
+//=================
+// State
+
+namespace vine {
+    // declare variable of this type in global scope to specify program deafult machine
+    // should be used once per program
+    struct default_machine_link {
+        default_machine_link(
+            const struct machine& machine
+        );
+    };
+
+    // sets machine to be executed after the current finishes
+    void set_machine(const machine&);
+
+    // request program shutdown
+    void request_shutdown();
+}
+
+//=================
+// Threads
+
+namespace vine {
+    // returns amount of thread workers
+    unsigned int get_threads_amount();
+
+    // returns id of current thread
+    // the id is in range (0 <= x < get_threads_amount())
+    unsigned int get_thread_id();
+}
+
 //=================
 // Stage
 
@@ -62,6 +94,27 @@ namespace vine {
 }
 
 //=================
+// Batch
+
+namespace vine {
+    // batch creates given container for each vine worker thread
+    // you can sync it's information by iterating on it's containers
+    template<class container>
+    struct batch {
+    private:
+        std::vector<container> containers;
+    public:
+        batch();
+
+        // returns container assigned to this thread
+        container&              get_local_container();
+
+        // returns list of all batch containers
+        std::vector<container*> get_all_containers();
+    };
+}
+
+//=================
 // Tasks
 
 namespace vine {
@@ -71,7 +124,7 @@ namespace vine {
         struct implementation;
         implementation* impl;
 
-        task_promise() { impl = nullptr; };
+        task_promise() : impl(nullptr) {};
         ~task_promise();
 
         task_promise(const task_promise&);
@@ -85,23 +138,25 @@ namespace vine {
     task_promise issue_task(task task, std::any arg);
 };
 
+#undef DELETE_MOVE_COPY
+
 //=================
-// State
+// Impl
 
-namespace vine {
-    // declare variable of this type in global scope to specify program deafult machine
-    // should be used once per program
-    struct default_machine_link {
-        default_machine_link(
-            const machine& machine
-        );
-    };
-
-    // sets machine to be executed after the current finishes
-    void set_machine(const machine&);
-
-    // request program shutdown
-    void request_shutdown();
+template<class container>
+vine::batch<container>::batch() {
+    auto threads = get_threads_amount();
+    containers.resize(threads);
 }
 
-#undef DELETE_MOVE_COPY
+template<class container>
+container& vine::batch<container>::get_local_container() {
+    return containers[get_thread_id()];
+}
+
+template<class container>
+std::vector<container*> vine::batch<container>::get_all_containers() {
+    std::vector<container*> res;
+    for (auto& c : containers) res.push_back(&c);
+    return res;
+}
